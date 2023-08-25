08d3e2e3edf8575e994b1adf50b3115bb7c81c97
FelixGomes <gustavogomes50@gmail.com>
2023-08-25 14:53:41 -0400

    Added login, router and shared modules, added login and loading pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_money_app/src/home/page/home_page.dart';
+import 'package:my_money_app/src/modules/initial/page/initial_page.dart';
+import 'package:my_money_app/src/modules/login/page/login_page.dart';
+import 'package:my_money_app/src/router/app_router.dart';
 
 void main() {
   runApp(const App());
@@ -8,24 +10,20 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Money App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        useMaterial3: true,
       ),
-      home: const HomePage(title: 'My Money App'),
+      home: const InitialPage(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      routes: {
+        AppRouter.login: (context) => const LoginPage(),
+      },
     );
   }
 }
